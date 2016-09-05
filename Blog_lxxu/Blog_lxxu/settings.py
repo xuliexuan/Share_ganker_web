0e80042b0743969574e157b6083881db9a4dccf4
ganker <599244887@qq.com>
2016-09-05 19:28:15 +0800

    test

--- Blog_lxxu/Blog_lxxu/settings.py
+++ Blog_lxxu/Blog_lxxu/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'article',
-    'statics',
+    'static',
 )
 
 # add bootstrap_admin
@@ -113,7 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 TEMPLATE_DIRS = (
--- Blog_lxxu/run_server.sh
+++ Blog_lxxu/run_server.sh
@@ -1,5 +1,5 @@
 # @Author: ganker
 # @Date:   2016-08-29 00:10:38
 # @Last Modified by:   ganker
-# @Last Modified time: 2016-08-29 00:38:29
-python manage.py runserver
+# @Last Modified time: 2016-08-31 14:22:20
+python manage.py runserver 0.0.0.0:8000
